6e1fab9a7e6ef976d93a095b7bc17075c42c7ebe
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-10-07 21:21:07 +0300

    add array of builtins' pointers, executing more than one command

--- main.c
+++ main.c
@@ -1,29 +1,59 @@
 #include "minishell.h"
 
-int	execute_commands(char **args)
+int cmd_echo(char **args, char **envp)
 {
-	int		status;
-	pid_t	pid;
-	pid_t	wpid;
-
-	pid = fork();
-	if (pid == 0)
-	{
-		args[0] = ft_strjoin("/bin/", args[0]);
-		if (execve(args[0], args, NULL) == -1)
-			ft_putendl_fd("Command not found", 1);
-		exit(EXIT_FAILURE);
-	}
+	if (ft_strncmp(args[1], "-n", 2) == 0)
+		ft_putstr_fd(args[2], 1);
 	else
-	{
-		wpid = waitpid(pid, &status, WUNTRACED);
-		while (!WIFEXITED(status) && !WIFSIGNALED(status))
-			wpid = waitpid(pid, &status, WUNTRACED);
-	}
-	return (1);
-
+		ft_putendl_fd(args[1], 1);
+	return(1);
 }
-static void print_invite()
+
+int		(*launch_builtin(int i))(char **args, char **envp)
+{
+	int (*launch_builtin[BUILTIN_NUM])(char **args, char **envp);
+
+	launch_builtin[ECHO] = &cmd_echo;
+	//launch_builtin[CD] = &cmd_cd;
+	//launch_builtin[PWD] = &cmd_pwd;
+	//launch_builtin[EXPORT] = &cmd_export;
+	//launch_builtin[UNSET] = &cmd_unset;
+	//launch_builtin[ENV] = &cmd_env;
+	//launch_builtin[EXIT] = &cmd_exit;
+	return (launch_builtin[i]);
+}
+
+/*
+ *  TODO: заменить strncmp на strcmp для точной проверки на равенство.
+ */
+
+int	execute(char *command, char **envp)
+{
+	char	**args;
+	int		status;
+
+	args = split_line(command);
+	status = 1;
+	if (ft_strncmp(args[0], "echo,", 4) == 0)
+		status = launch_builtin(ECHO)(args, envp);
+	else if (ft_strncmp(args[0], "cd,", 2) == 0)
+		status = launch_builtin(CD)(args, envp);
+	else if (ft_strncmp(args[0], "pwd,", 3) == 0)
+		status = launch_builtin(PWD)(args, envp);
+	else if (ft_strncmp(args[0], "export,", 6) == 0)
+		status = launch_builtin(EXPORT)(args, envp);
+	else if (ft_strncmp(args[0], "unset,", 5) == 0)
+		status = launch_builtin(UNSET)(args, envp);
+	else if (ft_strncmp(args[0], "env,", 3) == 0)
+		status = launch_builtin(ENV)(args, envp);
+	else if (ft_strncmp(args[0], "exit,", 4) == 0)
+		status = launch_builtin(EXIT)(args, envp);
+	else
+		ft_putendl_fd("Command not found", 1);
+	return (status);
+}
+
+static void print_prompt()
 {
 	char buf[200];
 
@@ -31,30 +61,34 @@ static void print_invite()
 	ft_putstr_fd("\033[1m \033[31m minishell:", 1);
 	ft_putstr_fd(buf, 1);
 	ft_putstr_fd("$ \033[0m", 1);
-
 }
-void		shell_loop(void)
+
+void		shell_loop(char **envp)
 {
 	char	*line;
-	char	**args;
+	char	**commands;
 	int		status;
 
 	status = 1;
 	while (status)
 	{
-		print_invite();
+		print_prompt();
 		get_next_line(0, &line);
-		args = split_line(line);
-		status = execute_commands(args);
+		commands = ft_split(line, ';');
+		while (status && commands && *commands)
+		{
+			status = execute(*commands, envp);
+			commands++;
+		}
 		free_gc(line);
 		line = NULL;
 	}
 	free_gc(line);
 }
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
-	shell_loop();
+	shell_loop(envp);
 
-	return(1);
+	return(0);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: kdahl <kdahl@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 20:15:32 by kdahl             #+#    #+#             */
-/*   Updated: 2020/10/05 21:54:16 by mtriston         ###   ########.fr       */
+/*   Updated: 2020/10/07 20:55:43 by mtriston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,16 @@
 # include <unistd.h>
 # include "libft/libft.h"
 
-int     main(int argc, char **argv);
+# define BUILTIN_NUM 7
+# define ECHO 0
+# define CD 1
+# define PWD 2
+# define EXPORT 3
+# define UNSET 4
+# define ENV 5
+# define EXIT 6
+
+int		main(int argc, char **argv, char **envp);
 char    **split_line(char const *s);
 
 #endif
