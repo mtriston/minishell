21a1f4a380c051487198920b1ac74449bc4f8b68
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-11-08 14:48:16 +0300

    Add exit if Ctrl+D is pressed in blank command line

--- builtins.c
+++ builtins.c
@@ -6,7 +6,7 @@
 /*   By: mtriston <mtriston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 13:58:22 by mtriston          #+#    #+#             */
-/*   Updated: 2020/11/07 18:11:03 by mtriston         ###   ########.fr       */
+/*   Updated: 2020/11/07 18:19:59 by mtriston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int cmd_echo(t_cmd *cmd, char **envp)
 	int		n_flag;
 	size_t	i;
 
+	(void)envp;
 	n_flag = 0;
 	i = 0;
 	if (cmd->args[0] != NULL)
@@ -54,7 +55,7 @@ int cmd_cd(t_cmd *cmd, char **envp)
 
 	if (envp_len(cmd->args) > 2)
 	{
-		ft_perror("cd: too many arguments");
+		ft_perror("cd: too many arguments", 1);
 		return (FAILURE);
 	}
 	if (cmd->args[1] == NULL)
@@ -68,7 +69,7 @@ int cmd_cd(t_cmd *cmd, char **envp)
 		dir = cmd->args[1];
 	if (chdir(dir) != 0)
 	{
-		ft_perror("cd");
+		ft_perror("cd", 1);
 		return (FAILURE);
 	}
 	//cmd_export("PWD", getcwd(NULL, 0), envp);
@@ -77,6 +78,7 @@ int cmd_cd(t_cmd *cmd, char **envp)
 
 int	cmd_env(t_cmd *cmd, char **envp)
 {
+	(void)cmd;
 	while (*envp)
 	{
 		ft_putendl_fd(*envp, 1);
@@ -89,7 +91,8 @@ int	cmd_exit(t_cmd *cmd, char **envp)
 {
 	int status;
 
-	if (cmd->args[1])
+	(void)envp;
+	if (cmd && cmd->args[1])
 		status = ft_atoi(cmd->args[1]);
 	else
 		status = 0;
@@ -101,6 +104,8 @@ int	cmd_pwd(t_cmd *cmd, char **envp)
 {
 	char buf[PATH_MAX];
 
+	(void)cmd;
+	(void)envp;
 	getcwd(buf, PATH_MAX);
 	ft_putendl_fd(buf, 1);
 	return (SUCCESS);
--- lexer/read_line.c
+++ lexer/read_line.c
@@ -6,7 +6,7 @@
 /*   By: mtriston <mtriston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 14:00:33 by mtriston          #+#    #+#             */
-/*   Updated: 2020/11/07 18:40:52 by mtriston         ###   ########.fr       */
+/*   Updated: 2020/11/08 14:29:21 by mtriston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@ char	*read_line(void)
 {
 	char	*line;
 	int		status;
+	int		ret;
 
 	line = NULL;
 	status = CONTINUE_INPUT;
 	while (status == CONTINUE_INPUT)
 	{
-		if (get_next_line(0, &line) == -1)
+		ret = get_next_line(0, &line);
+		if (ret == -1)
 			return (NULL);
 		if (line == NULL)
 			continue;
+		if (ft_strlen(line) == 0 && ret == 0)
+			cmd_exit(NULL, NULL);
 		status = validate_line(line);
 		if (status == SYNTAX_ERROR)
 		{
